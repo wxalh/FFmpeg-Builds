53cb12f3a527c337c1eb8260a5cc2a5eacf7d880
wx <751116023@qq.com>
2026-02-12 17:40:23 +0800

    修复bug

--- variants/defaults-gpl.sh
+++ variants/defaults-gpl.sh
@@ -1,5 +1,10 @@
-FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug"
-FF_CFLAGS=""
+FF_CONFIGURE="--enable-gpl --enable-version3"
+
+if [[ $TARGET == win32 ]]; then
+    FF_CONFIGURE+=" --disable-debug --disable-w32threads --enable-pthreads --disable-filter=gfxcapture"
+fi
+
+FF_CFLAGS="-D_WIN32_WINNT=0x0601 -DWINVER=0x0601"
 FF_CXXFLAGS=""
 FF_LDFLAGS=""
 GIT_BRANCH="master"
